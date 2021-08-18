3c5ef4da763a309cb396f8c5f4cea3a82556cc09
Harshala-Gaikwad <59639322+Harshala-Gaikwad@users.noreply.github.com>
2021-08-18 08:42:05 +0530

    Add files via upload

---
+++ geeks_for_geeks/Array/addition_under_modulo.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+def sumUnderModulo(a,b):
+    # code here
+    return (a+b)%(10**9+7)
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        a,b = map(int,input().strip().split())
+        print(sumUnderModulo(a,b))
+
+# } Driver Code Ends
---
+++ geeks_for_geeks/Array/cyclically_rotate_array_by_one.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    n = int(input())
+    l = list(map(int,input().split()))
+    print(l[-1],end=" ")
+    for i in range(len(l)-1):
+        print(l[i],end=" ")
+    print()    
